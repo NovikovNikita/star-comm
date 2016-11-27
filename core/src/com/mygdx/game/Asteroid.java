0f2f57f469aa2c1c90c11986ce5f608de7a43739
Nikita <nothanks000@gmail.com>
2016-11-27 17:50:45 +0300

    some changes

--- core/src/com/mygdx/game/Asteroid.java
+++ core/src/com/mygdx/game/Asteroid.java
@@ -10,7 +10,6 @@ public class Asteroid {
     private Vector2 position;
     private float speed;
     private Rectangle rect;
-    private int hp;
     private float ang;
     private float size;
 
@@ -21,10 +20,9 @@ public class Asteroid {
     public Asteroid() {
         if (texture == null)
             texture = new Texture("asteroid60.tga");
-        speed = 6.0f + (float) Math.random() * 3.0f;
-        position = new Vector2(1280.0f + (float) Math.random() * 1280.0f, (float) Math.random() * 720.0f);
+        speed = 1.0f + (float) Math.random() * 9.0f;
+        position = new Vector2(1280.0f + (float) Math.random() * 640.0f, (float) Math.random() * 720.0f);
         rect = new Rectangle(position.x, position.y, 60, 60);
-        hp = 3;
         ang = (float) Math.random() * 360.0f;
         size = 0.8f + (float)Math.random() * 0.4f;
     }
@@ -32,7 +30,7 @@ public class Asteroid {
     public void render(SpriteBatch batch) {
         // batch.setColor(1, 1 - (3 - hp) * 0.3f, 1 - (3 - hp) * 0.3f, 1);
         //batch.draw(texture, position.x, position.y);
-        batch.draw(texture, position.x, position.y, 30, 30, 60, 60, size, size, ang, 0, 0, 60, 60, false, false);
+        batch.draw(texture, position.x, position.y, 30, 30, 60, 60, 1.0f, 1.0f, ang, 0, 0, 60, 60, false, false);
         // batch.setColor(1, 1, 1, 1);
     }
 
@@ -40,25 +38,17 @@ public class Asteroid {
         position.x = 1280.0f + (float) Math.random() * 1280;
         position.y = (float) Math.random() * 720;
         speed = 6.0f + (float) Math.random() * 3.0f;
-        hp = 3;
         ang = (float) Math.random() * 360.0f;
         size = 0.5f + (float)Math.random() * 1.0f;
     }
 
-    public void getDamage(int dmg) {
-        hp -= dmg;
-        if (hp <= 0) {
-            recreate();
-        }
-    }
-
     public void update() {
         position.x -= speed;
-        rect.x = position.x;
-        rect.y = position.y;
-        ang += speed / 2;
+        ang += speed;
         if (position.x < -60) {
             recreate();
         }
+        rect.x = position.x;
+        rect.y = position.y;
     }
 }
--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -10,11 +10,13 @@ public class MyGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Background background;
 	Hero hero;
-	private final int AST_COUNT = 50;
+	private final int AST_COUNT = 20;
 	Asteroid[] asteroids;
-	final int BULLETS_COUNT = 20;
+	final int BULLETS_COUNT = 50;
 	public  static Bullet[] bullets;
 	private Texture texBullet;
+	private final int MAX_FXES = 20;
+	public static MyFX[] fxes;
 
 	
 	@Override
@@ -31,7 +33,10 @@ public class MyGdxGame extends ApplicationAdapter {
 			bullets[i] = new Bullet();
 		}
 		texBullet = new Texture("bullet20.png");
-
+		fxes = new MyFX[MAX_FXES];
+		for (int i = 0; i < MAX_FXES; i++) {
+			fxes[i] = new MyFX();
+		}
 	}
 
 	@Override
@@ -48,6 +53,9 @@ public class MyGdxGame extends ApplicationAdapter {
 		for (int i = 0; i < BULLETS_COUNT; i++) {
 			if (bullets[i].isActive())
 				batch.draw(texBullet, bullets[i].getPosition().x, bullets[i].getPosition().y);
+		}
+		for (int i = 0; i < MAX_FXES; i++) {
+			fxes[i].render(batch);
 		}
 		batch.end();
 	}
@@ -66,17 +74,23 @@ public class MyGdxGame extends ApplicationAdapter {
 			if (bullets[i].isActive()) {
 				for (int j = 0; j < AST_COUNT; j++) {
 					if (asteroids[j].getRect().contains(bullets[i].getPosition())) {
-						asteroids[i].getDamage(1);
+						asteroids[i].recreate();
 						bullets[i].destroy();
+
+						for (int k = 0; k < MAX_FXES; k++) {
+							if (!fxes[k].isActive()) {
+								fxes[k].setup(bullets[i].getPosition().x, bullets[i].getPosition().y);
+								break;
+							}
+						}
+
 						break;
 					}
 				}
 			}
 		}
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
+		for (int i = 0; i < MAX_FXES; i++) {
+			fxes[i].update();
+		}
 	}
 }
